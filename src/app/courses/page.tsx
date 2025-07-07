e644eb33a2793d738fae684c96e46991ac1a392c
Arghya Dhar <arghya1956@gmail.com>
2025-07-07 08:44:25 +0000

    add this B.tech ME college IEM Salt Lake & Newtown/Manipal University /

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -291,6 +291,29 @@ const btechEeColleges = [
     'Graphic Era Deemed to be University',
 ];
 
+const btechMeColleges = [
+    'IEM Salt Lake & Newtown',
+    'Manipal University',
+    'Adamas University',
+    'Techno Main Salt Lake & Newtown',
+    'Heritage Institute of Technology',
+    'Amity University, Kolkata',
+    'MCKV Institute of Engineering',
+    'Swami Vivekananda University (SVU)',
+    'JIS College of Engineering, Kalyani',
+    'Supreme Knowledge Foundation',
+    'KIIT',
+    'SOA',
+    'AMITY NOIDA',
+    'JAIN (Deemed-to-be University)',
+    'Sathyabama University',
+    'Galgotias University',
+    'Sarhad University',
+    'Shiv Nadar Institution of Eminence',
+    'Shivalik College, Dehradun',
+    'Graphic Era Deemed to be University',
+];
+
 
 const courseData: { [key: string]: Course[] } = {
   engineering: [
@@ -306,7 +329,7 @@ const courseData: { [key: string]: Course[] } = {
     { name: 'B.Tech. in Electronics and Communication Engineering (ECE)', colleges: btechEceColleges },
     { name: 'B.Tech. in Electrical and Electronics Engineering (EEE)', colleges: btechEeeColleges },
     { name: 'B.Tech. in Electrical Engineering (EE)', colleges: btechEeColleges },
-    { name: 'B.Tech. in Mechanical Engineering (ME)', colleges: [] },
+    { name: 'B.Tech. in Mechanical Engineering (ME)', colleges: btechMeColleges },
     { name: 'B.Tech. in Biotechnology', colleges: [] },
     { name: 'B.Tech. in Biomedical Engineering', colleges: [] },
     { name: 'B.Tech. in Agricultural Engineering', colleges: [] },
