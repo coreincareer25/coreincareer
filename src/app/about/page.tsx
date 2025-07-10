9dc79e9836b12c998c52aa28defd2fdb50b4a205
Arghya Dhar <arghya1956@gmail.com>
2025-07-10 10:54:12 +0000

    change the placeholder to jpg file in our story and vision in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -77,7 +77,7 @@ export default function AboutPage() {
                         >
                             <div className="relative h-96 w-full overflow-hidden rounded-lg shadow-lg">
                                 <Image
-                                    src="https://placehold.co/600x400.png"
+                                    src="https://placehold.co/600x400.jpg"
                                     alt="Career counseling session"
                                     fill
                                     className="object-cover"
