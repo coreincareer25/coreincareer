3c3cd99f23538f6ceb965d7c7ac29b60827a57f1
Arghya Dhar <arghya1956@gmail.com>
2025-07-11 16:55:20 +0000

    how can i different changes of colleges in college page

---
+++ src/lib/college-data.ts
@@ -0,0 +1,90 @@
+export const collegeData = {
+    engineering: {
+      title: 'Engineering',
+      colleges: [
+        'IEM Salt Lake', 'IEM Newtown', 'Manipal University', 'Adamas University', 'Sister Nivedita University', 'Techno India Salt Lake', 'Heritage Institute of Technology, Kolkata', 'Amity University Kolkata', 'Kalinga Institute of Industrial Technology', 'SIKSHA `O` ANUSANDHAN', 'JAIN (Deemed-to-be University)', 'JIS University', 'National Institutes of Technology', 'MCKV Institute of Engineering', 'B. P. Poddar Institute of Management & Technology', 'Galgotias University', 'Shivalik College, Dehradun', 'Sharda University', 'Brainware University', 'Swami Vivekananda University Kolkata', 'JIS College of Engineering Kalyani', 'Guru Nanak Institute of Technology', 'Dr. Sudhir Chandra Sur Institute of Technology & Sports Complex', 'Supreme Knowledge Foundation', 'Abacus Institute of Engineering and Management'
+      ]
+    },
+    medical: {
+      title: 'Medical',
+      subCategories: {
+        mbbs: {
+          title: 'Bachelor of Medicine, Bachelor of Surgery (MBBS)',
+          colleges: ['JMN Medical College and Hospital', 'IQ City Medical College Hospital', 'Jagannath Gupta Institute of Medical Sciences And Hospital', 'GOURI DEVI INSTITUTE OF MEDICAL SCIENCES AND HOSPITAL', 'KPC Medical College & Hospital', 'Shri Ramkrishna Institute of Medical Sciences', 'Saraswathi Institute of Medical Sciences', 'Christian Medical College & Hospital']
+        },
+        bds: {
+          title: 'Bachelor of Dental Surgery (BDS)',
+          colleges: ['Guru Nanak Institute of Dental Sciences & Research', 'Kusum Devi Sunderlal Dugar Jain Dental College & Hospital', 'Haldia Institute of Dental Sciences & Research (HIDSAR)', 'Dr R Ahmed Dental College and Hospital', 'Manav Rachna Dental College', 'Al-Azhar Dental College Thodupuzha']
+        }
+      }
+    },
+    computational: {
+      title: 'Computational',
+      subCategories: {
+        ug: {
+          title: 'UG - BCA, Animation & Grafix, Data Science, Cyber Security',
+          colleges: ['EIILM Kolkata', 'Swami Vivekananda University', 'IEM', 'Techno India Salt Lake', 'Guru Nanak Institute of Technology', 'JIS College of Engineering Kalyani', 'Brainware University', 'Sister Nivedita University', 'iLEAD Kolkata', 'NSHM Knowledge Campus Kolkata', 'Adamas University', 'Supreme Knowledge Foundation', 'Amity University Kolkata', 'JIS University', 'ABACUS Institute of Engineering and Management', 'MCKV Institute of Engineering']
+        },
+        pg: {
+          title: 'PG - MCA',
+          colleges: ['IEM', 'JIS University', 'Brainware University', 'Adamas University', 'Techno India Salt Lake', 'GNIT', 'Heritage Institute of Technology, Kolkata', 'Future Institute of Engineering and Management']
+        }
+      }
+    },
+    management: {
+      title: 'Management',
+      subCategories: {
+        ug: {
+          title: 'UG - BBA, Hospital Management, Hotel and Hospitality Management, Journalism and Media Science, Airlines',
+          colleges: ['EIILM Kolkata', 'Swami Vivekananda University', 'IEM', 'Techno India Salt Lake', 'Bengal School of Technology & Management', 'Kalinga Institute of Industrial Technology', 'Siksha `O` Anusandhan', 'JAIN (Deemed-to-be University)', 'IIHM', 'Guru Nanak Institute of Hotel Management', 'IEMs International Institute of Hotel Management', 'Shardha University', 'Brainware University', 'Sister Nivedita University', 'ILEAD', 'NSHM Knowledge Campus Kolkata', 'Adamas University', 'Amity University Kolkata', 'JIS University', 'MCKV Institute of Engineering', 'ABACUS Institute of Engineering and Management', 'Supreme Knowledge Foundation', 'ISHM']
+        },
+        pg_wb: {
+          title: 'PG - MBA (West Bengal)',
+          colleges: ['EIILM Kolkata', 'IEM', 'Techno India Salt Lake', 'Bengal Institute of Business Studies', 'ASMi Business School', 'Globsyn Business School', 'Adamas University', 'Praxis Business School', 'International Management Institute', 'St Xaviers University', 'Swami Vivekananda University', 'MCKV Institute of Engineering', 'Sister Nivedita University', 'Brainware University']
+        },
+        pg_bgl: {
+          title: 'PG - MBA (Bangalore)',
+          colleges: ['Kirloskar Institute of Advanced Management Studies', 'Regional College of Management Bangalore', 'IIBS International Institute of Business Study Bengaluru', 'ABBS Autonomous College, Bengaluru', 'IZee Business School', 'JAIN (Deemed-to-be University)', 'GIBS Business School, Bangalore Campus']
+        },
+        pg_mum: {
+          title: 'PG - MBA (Mumbai / Pune)',
+          colleges: ['Ramachandran International Institute Of Management', 'Pune Institute of Business Management', 'ISMS Pune', 'ITM Business School Navi Mumbai', 'Universal Business School', 'Dr. D. Y. Patil Vidyapeeth, Pune']
+        },
+        pg_del: {
+          title: 'PG - MBA (Delhi NCR)',
+          colleges: ['FOSTIIMA Business School', 'Jaipuria Institute of Management', 'IILM Institute for Higher Education', 'Asia Pacific Institute of Management (APIM)']
+        },
+        pg_hyd: {
+          title: 'PG - MBA (Hyderabad)',
+          colleges: ['Vignana Jyothi Institute Of Management', 'Siva Sivani Institute of Management']
+        }
+      }
+    },
+    healthcare: {
+      title: 'Health Care Technology & Science',
+      subCategories: {
+        main: {
+          title: 'Paramedical, B.Pharm, D.Pharm, Biotechnology, Microbiology, Food Technology, Nutrition, Agriculture',
+          colleges: ['Swami Vivekananda University', 'JIS University', 'Techno India Main Campus', 'Bengal School of Technology', 'Brainware University', 'iLEAD Kolkata', 'M R Institute of Nursing', 'Barasat College of Pharmaceutical Science & Research Centre', 'Adamas University', 'Sister Nivedita University', 'The Neotia University', 'Guru Nanak Institute of Pharmaceutical Science and Technology', 'NSHM Knowledge Campus', 'Supreme Knowledge Foundation']
+        }
+      }
+    },
+    llb: {
+      title: 'LLB',
+      subCategories: {
+        main: {
+          title: 'LLB, BA LLB, BBA LLB, B.com LLB',
+          colleges: ['Adamas University', 'JIS University', 'Swami Vivekananda University', 'Sister Nivedita University', 'Amity University', 'Brainware University', 'The Neotia University']
+        }
+      }
+    },
+    nursing: {
+      title: 'Nursing',
+      subCategories: {
+        main: {
+          title: '(B.Sc Nursing, GNM Nursing, ANM Nursing)',
+          colleges: ['Brainware University', 'Sister Nivedita University', 'M R Institute of Nursing', 'Kolkata Institute of Nursing', 'International Institute of Nursing & Research Kalyani']
+        }
+      }
+    }
+  };
